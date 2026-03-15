76f02d81963f54f2699f08db970af18f7adc0f15
jonayed-nasir <junayednasir073@gmail.com>
2026-03-15 12:03:15 +0600

    cache/client.py update

--- jwtauth/aiAgent/cache/client.py
+++ jwtauth/aiAgent/cache/client.py
@@ -2,6 +2,7 @@
 import redis
 
 # Connection Pool ব্যবহার করলে কানেকশন রিসাইকেল হয়, পারফরম্যান্স বাড়ে
+pool = redis.ConnectionPool(host='newsmartagent-redis', port=6379, decode_responses=False)
 pool = redis.ConnectionPool(
     host='newsmartagent-redis', 
     port=6379, 
