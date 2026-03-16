a2da2d2e1c358636d53e90a9b0986a6c80e98e9c
jonayed-nasir <junayednasir073@gmail.com>
2026-03-17 01:40:01 +0600

    dashboard ai update v-3

--- jwtauth/settings/admin.py
+++ jwtauth/settings/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import AgentSettings, GlobalSettings
 
-# Register your models here.
+admin.site.register(AgentSettings)
+
+@admin.register(GlobalSettings)
+class GlobalSettingsAdmin(admin.ModelAdmin):
+    # Only allow one instance to be edited
+    def has_add_permission(self, request):
+        if self.model.objects.exists():
+            return False
+        return super().has_add_permission(request)
+
+    def has_delete_permission(self, request, obj=None):
+        return False
