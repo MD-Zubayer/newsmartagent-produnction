9c128ce82cdd4abab1697b9638cc7427d1ba4018
jonayed-nasir <junayednasir073@gmail.com>
2026-03-17 23:13:01 +0600

    pwa v-3

--- nextjs/app/layout.js
+++ nextjs/app/layout.js
@@ -146,6 +146,24 @@ export default function RootLayout({ children }) {
         
         {children}
         <MessengerButton />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              if (typeof window !== "undefined" && "serviceWorker" in navigator) {
+                window.addEventListener("load", function () {
+                  navigator.serviceWorker.register("/sw.js").then(
+                    function (registration) {
+                      console.log("Service Worker registration successful with scope: ", registration.scope);
+                    },
+                    function (err) {
+                      console.log("Service Worker registration failed: ", err);
+                    }
+                  );
+                });
+              }
+            `,
+          }}
+        />
 
 
 <Toaster 
