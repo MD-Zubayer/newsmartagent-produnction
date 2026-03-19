9fd4a3f1f3d339391bd4a88b69b691e270fb0f68
jonayed-nasir <junayednasir073@gmail.com>
2026-03-19 13:56:57 +0600

    profile photo upload v-4

--- jwtauth/minio_management/storages.py
+++ jwtauth/minio_management/storages.py
@@ -12,8 +12,31 @@ class BaseMinioStorage(S3Boto3Storage):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def url(self, name, parameters=None, expire=None, http_method=None):
-        # Robust URL generation to avoid the 'missing colon' issue
+    def url(self, name, parameters=None, expire=900, http_method=None):
+        # Generate a signed URL if querystring_auth is True
+        if self.querystring_auth:
+            import boto3
+            from botocore.config import Config
+            
+            # We must use the EXTERNAL endpoint for the signature so the client can reach it
+            ext_endpoint = os.environ.get('MINIO_EXTERNAL_ENDPOINT', 'https://s3.newsmartagent.com')
+            
+            s3_client = boto3.client(
+                's3',
+                aws_access_key_id=self.access_key,
+                aws_secret_access_key=self.secret_key,
+                endpoint_url=ext_endpoint,
+                config=Config(signature_version='s3v4'),
+                region_name='us-east-1' # MinIO usually doesn't care about region but boto3 needs it
+            )
+            
+            return s3_client.generate_presigned_url(
+                'get_object',
+                Params={'Bucket': self.bucket_name, 'Key': name},
+                ExpiresIn=expire
+            )
+            
+        # Fallback to simple absolute URL for public buckets
         ext_endpoint = os.environ.get('MINIO_EXTERNAL_ENDPOINT', '').rstrip('/')
         if ext_endpoint:
             return f"{ext_endpoint}/{self.bucket_name}/{name}"
@@ -27,3 +50,4 @@ class BlogStorage(BaseMinioStorage):
 
 class ProfileStorage(BaseMinioStorage):
     bucket_name = os.environ.get('MINIO_PROFILE_BUCKET_NAME', 'newsmartagent-profile')
+    querystring_auth = True
