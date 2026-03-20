48007d1387b73eca85ccdb2f3f3ecae051798efe
jonayed-nasir <junayednasir073@gmail.com>
2026-03-20 20:41:08 +0600

    Web Chat Widget Implementation v-8

--- jwtauth/jwtauth/settings.py
+++ jwtauth/jwtauth/settings.py
@@ -211,6 +211,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = "/app/static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
