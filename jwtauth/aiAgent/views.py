9d73de4fd53830eefdaaef8eb83a3b6554ff3622
jonayed-nasir <junayednasir073@gmail.com>
2026-03-22 15:40:40 +0600

    mplemented the "Human Help" feature! v-5

--- jwtauth/aiAgent/views.py
+++ jwtauth/aiAgent/views.py
@@ -56,6 +56,26 @@ class AgentAIViewSet(viewsets.ModelViewSet):
         """
         return AgentAI.objects.filter(user=self.request.user).exclude(page_id__startswith='dashboard_').order_by('-created_at')
 
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        if not serializer.is_valid():
+            import logging
+            logger = logging.getLogger(__name__)
+            logger.error(f"AgentAI Update Validation Error: {serializer.errors}")
+            print(f"AgentAI Update Validation Error: {serializer.errors}")
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
 
 class UserAvailableModelsView(APIView):
     permission_classes = [IsAuthenticated]
