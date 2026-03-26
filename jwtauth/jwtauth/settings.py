6fcb6c6f96ddafd5cb2850e894a78307ad8d4271
jonayed-nasir <junayednasir073@gmail.com>
2026-03-26 11:05:46 +0600

    telegram integartion v-8

--- jwtauth/jwtauth/settings.py
+++ jwtauth/jwtauth/settings.py
@@ -168,7 +168,7 @@ CELERY_TASK_TIME_LIMIT = 110
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600
+        conn_max_age=60
     )
 }
 
